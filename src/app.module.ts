84fbe63ee4b789967978adba379b43df5cdd6ed9
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-10-15 16:52:49 +0300

    edited app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RestaurantModule } from './modules/restaurant.module';  
+
 
 @Module({
-  imports: [],
+  imports: [RestaurantModule],
   controllers: [AppController],
   providers: [AppService],
 })
