3d375b08d0ed4672afed0266bc646d7b09f2bbfa
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-10-22 13:05:52 +0300

    edited restaurant controller

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,12 +1,42 @@
-
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Post, Body, Render, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Request } from 'express';
+import { Express } from 'express'; 
+import { RestaurantService } from '../services/restaurant.service';  // Import the service
 
 @Controller()
 export class RestaurantController {
-    @Get('/restaurantregistration')
-    @Render('restaurant-register')  
-    showRegistrationPage() {
-        console.log("Restaurant registration page requested.");
+  constructor(private readonly restaurantService: RestaurantService) {}  // Inject the service
+
+  @Get('/restaurantregistration')
+  @Render('restaurant-register')
+  showRegistrationPage() {
     return {};
-    }
+  }
+
+  @Post('/submit-registration')
+@UseInterceptors(FileInterceptor('photo'))
+async registerRestaurant(
+  @Body() body: Request['body'],
+  @UploadedFile() file: Express.Multer.File
+) {
+  const { establishmentName, email, phone, socialMedia, website } = body;
+
+  let photoPath = null;
+  try {
+    
+    // if (file) {
+    //   photoPath = await this.restaurantService.uploadPhoto(file);
+    // }
+
+    // Insert restaurant data
+    await this.restaurantService.insertRestaurantData(establishmentName, email, phone, socialMedia, website, photoPath);
+
+    return { success: true };
+  } catch (error) {
+    return { error: error.message };
+  }
+}
+
+  
 }
