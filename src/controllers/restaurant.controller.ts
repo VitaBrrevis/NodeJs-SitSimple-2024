fba89f2e79a6f44c8d3dc288fb3a130e7522cc2f
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-10-22 13:50:54 +0300

    added redirect to homepage

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Post, Body, Render, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Render, UploadedFile, UseInterceptors, Redirect, Res } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { Request } from 'express';
 import { Express } from 'express'; 
 import { RestaurantService } from '../services/restaurant.service';  
 
 @Controller()
 export class RestaurantController {
-  constructor(private readonly restaurantService: RestaurantService) {}  
+  constructor(private readonly restaurantService: RestaurantService) {}
 
   @Get('/restaurantregistration')
   @Render('restaurant-register')
@@ -15,31 +16,26 @@ export class RestaurantController {
   }
 
   @Post('/submit-registration')
-@UseInterceptors(FileInterceptor('photo'))
-async registerRestaurant(
-  @Body() body: Request['body'],
-  @UploadedFile() file: Express.Multer.File
-) {
-  const { establishmentName, email, phone, socialMedia, website } = body;
+  @UseInterceptors(FileInterceptor('photo'))
+  async registerRestaurant(
+    @Body() body: Request['body'],
+    @UploadedFile() file: Express.Multer.File,
+    @Res() res: Response
+  ) {
+    const { establishmentName, email, phone, socialMedia, website } = body;
 
-  let photoPath = null;
-  try {
-    if (file) {
-        console.log('File detected. Proceeding to upload:', file.originalname); 
+    let photoPath = null;
+    try {
+      if (file) {
         photoPath = await this.restaurantService.uploadPhoto(file);
-      } else {
-        console.error('No file uploaded'); 
       }
 
-      console.log('Inserting restaurant data. Photo path:', photoPath); 
-    await this.restaurantService.insertRestaurantData(establishmentName, email, phone, socialMedia, website, photoPath);
+      await this.restaurantService.insertRestaurantData(establishmentName, email, phone, socialMedia, website, photoPath);
 
-    return { success: true };
-  } catch (error) {
-    console.error('Error in registerRestaurant:', error);
-    return { error: error.message };
+    
+      return res.redirect('/'); // потрібно буде змінити редірект на /booking коли буде готова ця сторінка
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
   }
-}
-
-  
 }
