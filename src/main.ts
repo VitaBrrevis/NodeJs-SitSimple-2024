e9636be38975629b06f5575b9a427bbf8bdf9c6e
Vita Doronina <v.doronina.ip23@kpi.ua>
2024-12-21 09:59:30 +0000

    Moving debug to the source code

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { join } from 'path';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+console.log('SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY);
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
