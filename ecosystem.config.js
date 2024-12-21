aa21e13700fc2fb9ad1be5bbd090ffc7e767acbe
Vita Doronina <v.doronina.ip23@kpi.ua>
2024-12-21 09:41:28 +0000

    Adding debug output of the env variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       instances: 'max', // Or a specific number of instances
       exec_mode: 'cluster', // Or 'fork' for single instance
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: 'development',
         SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
         SUPABASE_URL: process.env.SUPABASE_URL,
       },
