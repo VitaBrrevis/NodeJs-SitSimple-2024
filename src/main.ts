5c65db3b1c17402e38f328133857efaded52ef6a
Vita Doronina <v.doronina.ip23@kpi.ua>
2024-12-21 17:17:43 +0000

    Removing debug output and switching build to main branch

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import { join } from 'path';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-console.log('SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY);
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
