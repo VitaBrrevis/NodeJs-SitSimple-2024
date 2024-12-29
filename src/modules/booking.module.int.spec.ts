d8b51e8445fc985ab582bcb4c34511c494249641
Vita Doronina <v.doronina.ip23@kpi.ua>
2024-12-29 17:22:39 +0000

    Fixing intergration test

--- src/modules/booking.module.int.spec.ts
+++ src/modules/booking.module.int.spec.ts
@@ -4,43 +4,21 @@ import * as request from 'supertest';
 import { BookingModule } from './booking.module';
 import { BookingService } from '../services/booking.service';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config({ path: '.env' });
 
 describe('BookingModule (integration)', () => {
   let app: NestExpressApplication;
-  let bookingService = { getAllRestaurants: () =>
-  [
-    {
-      "id": 1,
-      "name": "Default Restaurant",
-      "contact_number": "123456789",
-      "email_address": "default@example.com",
-      "address_id": 2,
-      "social_media": "default_social",
-      "website": "http://default-website.com",
-      "photo_restaurant": "defaultrestaurant.jpg"
-    }
-  ],
-  getAllCities: () => [
-      {
-        "id": -1,
-        "city": "All cities"
-      },
-      {
-        "id": 2,
-        "city": "Gotham"
-      }
-      ]
-    };
 
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [BookingModule]
-    })
-      .overrideProvider(BookingService)
-      .useValue(bookingService)
-      .compile();
 
-    app = moduleFixture.createNestApplication<NestExpressApplication>();
+    const tmodule: TestingModule = await Test.createTestingModule({
+      imports: [BookingModule]
+    }).compile();
+
+    app = tmodule.createNestApplication<NestExpressApplication>();
     app.setViewEngine('pug');
     app.setBaseViewsDir(join(__dirname, '..', 'views'));
     await app.init();
